da90d287e1590d71cbbfdc0f3b28836a296c74c7
Bryan Nelson <bryananelsonjr@gmail.com>
2021-01-12 21:36:48 -0500

    Initial Commit

---
+++ Emoji Dictionary/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Emoji Dictionary
+//
+//  Created by Bryan Nelson on 1/12/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
