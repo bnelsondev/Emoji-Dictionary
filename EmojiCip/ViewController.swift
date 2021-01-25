eac44a6b713a389b0f05ce9745d5b6620c95f20e
Bryan Nelson <bryananelsonjr@gmail.com>
2021-01-24 21:39:02 -0500

    Added App Clip

---
+++ EmojiCip/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EmojiCip
+//
+//  Created by Bryan Nelson on 1/24/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
